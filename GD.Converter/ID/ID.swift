dbd6a845d5afec25f62ca939a135a98b1850b01a
Хизир Дудуев <hizir.duduev@gmail.com>
2019-03-27 06:55:47 +0300

    add ID

---
+++ GD.Converter/ID/ID.swift
@@ -0,0 +1,14 @@
+//
+//  ID.swift
+//  ExchangeRatesGD
+//
+//  Created by -=HIZIR=- on 11/03/2019.
+//  Copyright © 2019 GurobaDeveloper. All rights reserved.
+//
+import Foundation
+
+enum ID: String {
+	//Segue
+	case SgConverter_Currencies
+	case SgUnwingCurrencies_Converter
+}
