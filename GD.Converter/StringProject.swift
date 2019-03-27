fa21eec2e3a88eca70d0806665bbbd62f9951914
Хизир Дудуев <hizir.duduev@gmail.com>
2019-03-27 06:47:20 +0300

    addStringProject

---
+++ GD.Converter/StringProject.swift
@@ -0,0 +1,13 @@
+//
+//  StringProject.swift
+//  ExchangeRatesGD
+//
+//  Created by -=HIZIR=- on 18/03/2019.
+//  Copyright © 2019 GurobaDeveloper. All rights reserved.
+//
+
+import Foundation
+
+enum StringProject: String {
+	case alertNotNetwork = "Нет подключения к сети, подключите интернет и запустите приложение снова"
+}
