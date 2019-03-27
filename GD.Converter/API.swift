b90733be87b7a278fb3510e838ac2250b7245eef
Хизир Дудуев <hizir.duduev@gmail.com>
2019-03-27 06:57:15 +0300

    add API

---
+++ GD.Converter/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  ExchangeRatesGD
+//
+//  Created by -=HIZIR=- on 10/03/2019.
+//  Copyright © 2019 GurobaDeveloper. All rights reserved.
+//
+import Foundation
+
+enum API: String {
+	case convert    = "https://apilayer.net/api/convert"
+}
+
+
